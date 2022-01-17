14a84c27213eb567b3dc753700964c324617f613
Taiga Ozawa <taigaozawa@gmail.com>
2022-01-18 05:02:00 +0900

    gitignore

--- next-polls/pages/api/polls/index.ts
+++ next-polls/pages/api/polls/index.ts
@@ -1,4 +1,14 @@
 import {NextApiRequest, NextApiResponse} from 'next';
+import {initializeApp, applicationDefault, cert} from 'firebase-admin/app';
+import {getFirestore, Timestamp, FieldValue} from 'firebase-admin/firestore';
+const serviceAccount = require('../../../serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch(req.method) {
